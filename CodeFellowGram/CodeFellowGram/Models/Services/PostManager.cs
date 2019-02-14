1e143029515876d8d355a3d4b481a5f3b6a9311f
mbgoseco <mbgoseco@gmail.com>
2019-02-13 23:57:23 -0800

    crud operations built in service

--- CodeFellowGram/CodeFellowGram/Models/Services/PostManager.cs
+++ CodeFellowGram/CodeFellowGram/Models/Services/PostManager.cs
@@ -16,25 +16,55 @@ namespace CodeFellowGram.Models.Services
         {
             _context = context;
         }
-
-        public async Task DeleteAsync(int id)
-        {
-            throw new NotImplementedException();
-        }
-
+        
+        /// <summary>
+        /// Finds a post in the Post database with a matching primary key, stores it as a new Post type variable, and returns it.
+        /// </summary>
+        /// <param name="id">Primary Key value</param>
+        /// <returns>Matching post or null</returns>
         public async Task<Post> FindPost(int id)
         {
-            throw new NotImplementedException();
+            Post post = await _context.Posts.FirstOrDefaultAsync(p => p.ID == id);
+
+            return post;
         }
 
+        /// <summary>
+        /// Returns a list of all posts in the Post database.
+        /// </summary>
+        /// <returns>List of posts</returns>
         public async Task<List<Post>> GetPosts()
         {
             return await _context.Posts.ToListAsync();
         }
 
+        /// <summary>
+        /// After receiving a Post object from the form data on a page, it attempts to find a matching primary key in the database. If no match is found the given Post object is added to the table. If a match is found then the properties of the exsisting object are updated with the data from the form.
+        /// </summary>
+        /// <param name="Post">Post object created from form data</param>
+        /// <returns>New or updated Post object</returns>
         public async Task SaveAsync(Post Post)
         {
-            throw new NotImplementedException();
+            if (await _context.Posts.FirstOrDefaultAsync(m => m.ID == Post.ID) == null)
+            {
+                _context.Posts.Add(Post);
+            }
+            else
+            {
+                _context.Posts.Update(Post);
+            }
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            Post post = await _context.Posts.FirstOrDefaultAsync(p => p.ID == id);
+
+            if (post != null)
+            {
+                _context.Posts.Remove(post);
+                await _context.SaveChangesAsync();
+            }
         }
     }
 }
